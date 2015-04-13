12daa12896dfb4d21fe560ac553f425eaf2c779d
Katerina Hanson <katerina.hanson@gmail.com>
2015-04-12 19:46:08 -0700

    Tweeked mode

--- recipes/upstarts.rb
+++ recipes/upstarts.rb
@@ -2,7 +2,7 @@
 template "/etc/init/helloworld.conf" do
   owner "root"
   group "root"
-  mode '0644'
+  mode '0755'
   source "upstart_init.conf.erb"
   action :create
   variables node[:app][:inits][:helloworld]
