9c2fe1d20ef392c402cb33f21badb6768ba298ae
Katerina Hanson <katerina.hanson@gmail.com>
2015-04-18 22:27:56 -0700

    Virtual env as a variable

--- recipes/python_envs.rb
+++ recipes/python_envs.rb
@@ -1,27 +1,26 @@
 
 node[:app][:python_apps].each do |app|
-    virtual_env_path = "/home/vagrant/#{app['name']}_env"
-    python_virtualenv virtual_env_path do
+    python_virtualenv app.virtual_env_path do
         owner app.user
         group app.group
         action :create
     end
 
     bash "install app_#{app['name']}" do
-      code "#{virtual_env_path}/bin/pip install -r requirements.txt"
+      code "#{app['virtual_env_path']}/bin/pip install -r requirements.txt"
       cwd app.base_dir
       user app.user
     end
 
     bash "start project_#{app['name']}" do
-      code "#{virtual_env_path}/bin/django-admin.py startproject test_project"
+      code "#{app['virtual_env_path']}/bin/django-admin.py startproject test_project"
       ignore_failure true
       cwd app.base_dir
       user app.user
     end
 
     bash "setup project_#{app['name']}" do
-      code "#{virtual_env_path}/bin/python setup.py develop"
+      code "#{app['virtual_env_path']}/bin/python setup.py develop"
       cwd app.base_dir
       user app.user
     end
